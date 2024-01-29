869abab055a894445f1e4d7d528da078fd1f2246
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-01-29 18:20:05 -0300

    Add files via upload

---
+++ ifelse_operadores_condicionais.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - IF-ELSE</title>
+</head>
+
+<body>
+
+   <?php 
+
+        // == - Igual
+        // === - Identico
+        # != ou <> - Diferente
+        # !== - Não indenticos
+        // < - Menor
+        // > - Maior
+        // <= - Menor ou igual
+        // >= - Maior ou igual
+        
+        
+        if('a' == 'a') {
+            echo 'Verdadeiro';
+            echo 'Segunda instrução';
+            echo 'Terceira instrução';
+        }
+        
+
+   ?>
+
+</body>
+
+</html>
