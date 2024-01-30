568b27b80cf6b757312eabe6ef757a175034d9bb
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-01-30 19:41:34 -0300

    Update ifelse_praticando.php

--- ifelse_praticando.php
+++ ifelse_praticando.php
@@ -10,16 +10,23 @@
    <?php 
         
          $usuario_possui_cartao_loja = true;
-         $valor_compra = 101;
+         $valor_compra = 150;
 
          $valor_frete = 50;
-         $recebeu_desconto_frete = false;
+         $recebeu_desconto_frete = true;
          
-        if($usuario_possui_cartao_loja == true && $valor_compra >= 100) {
+        if($usuario_possui_cartao_loja == true && $valor_compra >= 400) {
             $valor_frete = 0;
-            $recebeu_desconto_frete = true;
-        }
 
+        } else if($usuario_possui_cartao_loja == true && $valor_compra >= 300){
+            $valor_frete = 10;
+
+        } else if($usuario_possui_cartao_loja == true && $valor_compra >= 100){
+            $valor_frete = 25;
+
+        } else {
+            $recebeu_desconto_frete = false;
+        }
    ?>
 
     <h1>Detalhes do pedido</h1>
