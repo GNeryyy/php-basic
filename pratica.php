2a2f3f6efc037d34aa3d62a3cb112dbff7001bf5
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-02-01 15:02:10 -0300

    Add files via upload

---
+++ pratica.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - Praticando</title>
+</head>
+
+<body>
+
+   <?php 
+
+       $idade = 18;
+       $altura = 1.50;
+
+       if ($idade >= 18 && $idade <= 60 && $altura >= 1.60 && $altura <= 2.00){
+
+                echo 'Está apito a frequentar o espaço';
+        
+       } else {
+                echo 'não está apito a frequentar o espaço';
+       } 
+
+       
+
+
+         
+   ?>
+
+    
+    
+
+
+</body>
+
+</html>
