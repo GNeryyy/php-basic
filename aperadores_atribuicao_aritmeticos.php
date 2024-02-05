92c3f8c6d6d81a2d310ddaa8138d45e91f399fc0
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-02-05 19:50:39 -0300

    Add files via upload

---
+++ aperadores_atribuicao_aritmeticos.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - Operadores aritmeticos - Atribuição</title>
+</head>
+
+<body>
+
+   <?php 
+
+       // =
+       // +, -, *, /, %
+
+       $x =10;
+       $y = 5;
+
+       //$x = $x + 5;
+       $x -= $y;
+
+       echo $x;
+
+   ?>
+
+</body>
+
+</html>
