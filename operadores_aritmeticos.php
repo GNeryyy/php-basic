8a2f20726161c626993b33df1d206220d94ab31d
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-02-05 19:29:29 -0300

    Add files via upload

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - Operadores aritmeticos</title>
+</head>
+
+<body>
+
+   <?php 
+
+       $num1 =8;
+       $num2 = 4;
+
+       echo "A soma entre $num1 e $num2 é " . ($num1 + $num2);
+
+       echo '<br>';
+       echo '<br>';
+
+       echo "A subtração entre $num1 e $num2 é " . ($num1 - $num2);
+
+       echo '<br>';
+       echo '<br>';
+
+       echo "A multiplicação entre $num1 e $num2 é " . ($num1 * $num2);
+
+       echo '<br>';
+       echo '<br>';
+
+       echo "A divisão entre $num1 e $num2 é " . ($num1 / $num2);
+
+       echo '<br>';
+       echo '<br>';
+       
+       echo "O modulo entre $num1 e $num2 é " . ($num1 % $num2);
+
+
+   ?>
+
+</body>
+
+</html>
