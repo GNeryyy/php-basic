9bba1b49e6e673b1132829dd1e7e3692d49a3adf
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-02-21 12:42:01 -0300

    Add files via upload

---
+++ loops_do_while.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - Loops do While</title>
+</head>
+
+<body>
+
+   <?php 
+
+        $x =1;
+
+       do {
+            echo "X = $x <br>";
+
+            $x++; //criterip de parada
+
+            //continue;
+            //break;
+
+       } while($x < 9);
+
+      /* echo '<br>';
+
+       while ($x <9) {
+        echo 'Entrou no while';
+       }*/
+
+
+   ?>
+</body>
+
+</html>
