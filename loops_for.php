3bc10cdad6e67582d99eda0b62b6e368b11fe502
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-02-22 14:39:58 -0300

    Add files via upload

---
+++ loops_for.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - Loops FOR</title>
+</head>
+
+<body>
+
+   <?php 
+
+       /*
+
+        $x = 1;
+
+       while($x < 10) {
+
+       }
+
+       do {
+
+       } 
+       while();                                             
+       */
+
+       for($x = 11; $x <= 10; $x--) {
+            
+       /* if($x >= 20) {
+            break;
+        }*/
+        
+        
+        echo "$x <br>";
+       }
+
+   ?>
+</body>
+
+</html>
