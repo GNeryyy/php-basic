4430b676f3b705009938d3d7523684580554888c
Gabriel Nery <124085004+GNeryyy@users.noreply.github.com>
2024-02-22 15:16:35 -0300

    Add files via upload

---
+++ loops_foreach.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP - Loops foreach </title>
+</head>
+
+<body>
+
+   <?php 
+
+        $itens = array('sofá', 'mesa','cadeira','fogão', 'geladeira');
+
+        echo '<pre>';
+        print_r($itens);
+        echo '</pre>';
+       
+        foreach ($itens as $item) {
+            echo "$item";
+
+            if($item == 'mesa') {
+                echo '( Compre uma mesa e ganhe 25% de desconto na compra de 4 cadeiras)';
+            }
+
+            echo '<br>';
+        }
+        
+   ?>
+</body>
+
+</html>
